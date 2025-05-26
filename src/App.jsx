39c0ded41fc27dc559862063f5d3956696a8b952
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-05-26 23:36:41 +0100

    Fix: Edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from "./components/Header";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <div className='max-w-4l mx-auto bg-white text-gray-500 shadow-lg my-6 p-6'>
+
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

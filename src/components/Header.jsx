1bd2674ef5d1fda4bcbe98ab8f737ee42d19425b
Enijeshiku Eniola <144893678+Hnn3y@users.noreply.github.com>
2025-05-28 20:06:27 +0000

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
-const Header = () => {
-    return (
-        <div className="text-center p-4 border-b">
+import './styles/Header.css'
 
+const Header = () => {
+
+    return (
+        <div class-1=" text-center p-4 border-b">
+            <h1 className="text-3xl font-bold">Full Name</h1>
+            <div className='text-sm mt-2 space-x-4'>
+                <a href=""></a>
+            </div>
         </div>
     )
 }
